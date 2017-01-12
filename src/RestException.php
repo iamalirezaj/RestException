46fa0d99dab9c0e8d78547eae018b64f8b8cd780
Alireza Josheghani <josheghani.dev@gmail.com>
2017-01-12 23:11:49 +0330

    Remove isApiCall method

--- src/RestException.php
+++ src/RestException.php
@@ -118,25 +118,6 @@ trait RestException
     }
 
     /**
-     * Determines if request is an api call.
-     *
-     * If the request URI contains '/api/v'.
-     *
-     * @author Alireza Josheghani <josheghani.dev@gmail.com>
-     * @since  11 Nov 2016
-     * @param  Request $request
-     * @return bool
-     */
-    protected function isApiCall(Request $request)
-    {
-        if (strpos($request->getUri(), 'api') || $request->wantsJson()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
      * Get Trace in development mode
      *
      * @author Alireza Josheghani <josheghani.dev@gmail.com>
@@ -173,11 +154,6 @@ trait RestException
      */
     public function renderRestException(Request $request, Exception $exception)
     {
-
-        if (!$this->isApiCall($request)) {
-            return parent::render($request, $exception);
-        }
-
         return $this->getJsonResponseForException($exception);
     }
 
